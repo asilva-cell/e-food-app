4e6fea61e03b3f5818464338208e7cf47c65dacd
asilva-cell <achapellin@gmail.com>
2020-05-06 21:24:17 -0600

    setting up the router

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Directory from '../../components/directory/directory.component';
-import './homepage.style.scss'
+import Directory from "../../components/directory/directory.component";
+import "./homepage.style.scss";
 
-const HomePage = () => (
+const HomePage = ({ history }) => (
 	<div className="homepage">
-		<Directory/>
+		<Directory />
 	</div>
 );
 
