837d4e34ba1ae5d6d0f7db69ab7ed15870fd85f2
asilva-cell <achapellin@gmail.com>
2020-05-07 21:25:36 -0600

    routint to Hat page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import "./App.css";
+
 const HatsPage = () => (
 	<div>
 		<h1>HATS PAGE </h1>
