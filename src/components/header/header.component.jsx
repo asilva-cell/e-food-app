257f445284b1e0c3340ce729cb559ea246db9057
asilva-cell <achapellin@gmail.com>
2020-05-10 17:56:47 -0600

    redirect to home if user signs in

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
-import { ReactComponent as Logo } from "../../assets/logo.svg";
+
 import { auth } from "../../firebase/firebase.utils";
+
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+
 import "./header.style.scss";
 
 const Header = ({ currentUser }) => (
