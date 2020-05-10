07b55aaea62726cdde6f19c5dfaa948d944fd5b9
asilva-cell <achapellin@gmail.com>
2020-05-09 20:09:45 -0600

    connected firebase storage, saved current user

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,30 @@ const config = {
 	appId: "1:349198946149:web:68407f2a481a538536db8c"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	if (!userAuth) return;
+
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
+	const snapShot = await userRef.get();
+
+	if (!snapShot.exists) {
+		const { displayName, email } = userAuth;
+		const createAt = new Date();
+
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createAt,
+				...additionalData
+			});
+		} catch (error) {
+			console.log("Error creating user.", error.message);
+		}
+	}
+	return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
