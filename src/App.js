993b992de4b2368076e6ef4aafd58c2a5bea2d08
asilva-cell <achapellin@gmail.com>
2020-05-12 22:28:01 -0600

    added empty message to cart & link empty checkout page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import { createStructuredSelector } from "reselect";
 import HomePage from "./pages/homepage/homepage.component";
 import Header from "./components/header/header.component";
 import ShopPage from "./pages/shop/shop.component";
+import CheckoutPage from "../src/pages/checkout/checkout.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurentUser } from "./redux/user/user.selectors";
@@ -43,6 +45,7 @@ class App extends React.Component {
 				<Switch>
 					<Route exact path="/" component={HomePage} />
 					<Route path="/shop" component={ShopPage} />
+					<Route exact path="/checkout" component={CheckoutPage} />
 					<Route
 						exact
 						path="/signin"
@@ -54,6 +57,7 @@ class App extends React.Component {
 							)
 						}
 					/>
+					<Route exact path="/checkout" component={CheckoutPage} />
 				</Switch>
 			</div>
 		);
---
+++ src/pages/checkout/checkout.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./checkout.component.jsx";
+
+const CheckoutPage = () => <div>Checkout Page</div>;
+
+export default CheckoutPage;
