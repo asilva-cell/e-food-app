4223a349f1fd8a3b33f2de4ee8933c9f64ceed07
asilva-cell <achapellin@gmail.com>
2020-06-12 20:05:54 -0600

    app running locally

---
+++ build/precache-manifest.e7dfa965975aaea098c05ab8d804413d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a664d362c56449f24cd7e2a33cc4f49a",
+    "url": "/e-food-app/public/index.html"
+  },
+  {
+    "revision": "e80ba438221d2dc89da9",
+    "url": "/e-food-app/public/static/css/main.eecfd3fb.chunk.css"
+  },
+  {
+    "revision": "3a20ff6fa4f51b8ac008",
+    "url": "/e-food-app/public/static/js/2.0c7bd42e.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/e-food-app/public/static/js/2.0c7bd42e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e80ba438221d2dc89da9",
+    "url": "/e-food-app/public/static/js/main.46ac7d8f.chunk.js"
+  },
+  {
+    "revision": "0f87b0e2bb25ee0ba346",
+    "url": "/e-food-app/public/static/js/runtime-main.67c9f305.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/e-food-app/public/static/media/logo.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/e-food-app/public/static/media/shoppingBag.30f07071.svg"
+  }
+]);
